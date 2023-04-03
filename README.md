a716a2978337745a638c4bda8552791b574325d9
jusdepatate <jusdepatate@protonmail.com>
2023-04-03 10:59:32 +0200

    Cart try no 1

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ Example e-commerce page made with Flexbox and Express.js+ejs.
 - **GET** `/`: returns a product list page,
 - **GET** `/login`: returns a (non-functional) login page,
 - **GET** `/cart`: returns a (non-functional) cart page,
+- **GET** `/cart/:action/:id`: executes a certain action (`add`, `delete`) on user's cart,
 - **GET** `/manager`: returns a product manager page,
 - **GET**/**POST** `/manager/:action/:id`: executes a certain action (`update`, `delete`, `create`) on a given product id.
--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mysql from "mysql2";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import sleep from "sleep";
 
-// Setting up Express.JS + eJS + body-parser
+// Setting up Express.JS + eJS
 const app = express();
+
 app.set('view engine', 'ejs');
+
+// Setting up public/
 app.use(express.static('public'));
+// Setting up  cookies and sessions of max age "(1000 * 60 * 60 * 24)" = 24 hours
+app.use(session({ cookie: { maxAge: (1000 * 60 * 60 * 24) }, secret: 'EcOmMeRcE' }));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000;
 
-// Connecting to Database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'ecommerce',
-    port: 3306,
-    database: 'ecommerce',
-    password: 'ecommerce'
-})
+
+function createConnection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'ecommerce',
+        port: 3306,
+        database: 'ecommerce',
+        password: 'ecommerce'
+    });
+}
 
 // Acknowledging GET calls on /
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    // Connecting to Database
+    const connection = createConnection();
+
+    // If cart undefined, create it
+    if(!(req.session.cart)) {
+        req.session.cart = [];
+    }
+
     // Checking products from database to fill page
-    connection.query("SELECT * FROM products", (err, rows, fields) => {
-        res.render('pages/index', { products: rows })
+    connection.execute("SELECT * FROM products", (err, rows) => {
+        res.render('pages/index', {products: rows})
     })
 })
 
@@ -32,49 +51,130 @@ app.get('/login', (req, res) => {
     res.render('pages/login');
 })
 
+
 // Acknowledging GET calls on /cart
 app.get('/cart', (req, res) => {
-    res.render('pages/cart');
+    if(!(req.session.cart)) {
+        res.render("pages/cart", {cart: []});
+        return;
+    } else if (req.session.cart.length === 0) {
+        res.render("pages/cart", {cart: []});
+        return;
+    }
+
+    // Connecting to Database
+    const connection = createConnection();
+
+    // Creating empty cart, this array will host every item's id, name and price
+    let cartComplete = [];
+    // async / await
+    req.session.cart.forEach((item) => {
+        // Creating empty element
+        let element = {
+            id: item,
+            name: "",
+            price: 0,
+        }
+
+        // Checking on database
+        connection.execute("SELECT * FROM products WHERE id = ?", [item], (err, rows) => {
+            cartComplete.push(element);
+        });
+
+        while(cartComplete.length === 0) {
+            sleep.msleep(100);
+        }
+
+        console.log(cartComplete);
+    });
+})
+
+// Acknowledging GET calls on /cart/.../...
+app.get('/cart/:action/:id', (req, res) => {
+    // If cart undefined, create it
+    if(!(req.session.cart)) {
+        req.session.cart = [];
+    }
+
+
+    // If action asked is to add to cart, add item on session
+    if(req.params.action === "add") {
+        req.session.cart.push(req.params.id);
+    }
+
+    // If action asked is to delete from cart, remove item from session
+    if(req.params.action === "delete") {
+        // Create a new cart, find element user wants to remove
+        // push new cart on session
+        let newCart = [];
+
+        req.session.cart.forEach((item) => {
+            if(!(item === req.params.id)) {
+                newCart.push(item);
+            }
+        })
+
+        req.session.cart = newCart;
+    }
+
+    res.redirect('/');
 })
 
 // Acknowledging GET calls on /manager
-app.get('/manager', (req, res) => {
+app.get('/manager', async (req, res) => {
+    // Connecting to Database
+    const connection = createConnection();
+
     // Checking products from database to fill page
-    connection.query("SELECT * FROM products", (err, rows) => {
-        res.render('pages/manager/index', { products: rows });
+    connection.execute("SELECT * FROM products", (err, rows) => {
+        res.render('pages/manager/index', {products: rows});
     })
 })
 
 // Acknowledging GET calls on /manager/.../...
-app.get('/manager/:action/:id', (req, res) => {
+app.get('/manager/:action/:id', async (req, res) => {
+    // Connecting to Database
+    const connection = createConnection();
+
     // If action asked is delete, delete given id, using ? escapes string from unsafe characters
-    if(req.params.action === "delete") {
-        connection.query("DELETE FROM products WHERE id = ?", [req.params.id]);
+    if (req.params.action === "delete") {
+        connection.execute("DELETE FROM products WHERE id = ?", [req.params.id]);
 
         // Redirect user back
         res.redirect("/manager");
     }
 
     // If action asked is update, show update page
-    if(req.params.action === "update") {
-        connection.query("SELECT * FROM products WHERE id = ?", [req.params.id], (err, rows) => {
-            res.render('pages/manager/update', { product: rows[0] });
-        })
+    if (req.params.action === "update") {
+        connection.execute("SELECT * FROM products WHERE id = ?", [req.params.id], (err, rows) => {
+            res.render('pages/manager/update', {product: rows[0]});
+        });
+    }
+
+    // If action asked is create, create a new item
+    if (req.params.action === "create") {
+        connection.execute("INSERT INTO products (name, price, in_stock, image) VALUES ('New item', 0, 0, 'https://picsum.photos/215/324?grayscale&blur')");
+
+        res.redirect('/manager');
     }
 })
 
 // Acknowledging POST calls on /manager/.../...
-app.post('/manager/:action/:id', (req, res) => {
+app.post('/manager/:action/:id', async (req, res) => {
+    // Connecting to Database
+    const connection = createConnection();
+
     // If action asked is update, update
-    if(req.params.action === "update") {
+    if (req.params.action === "update") {
+        // Managing the weird way that browsers use to tell if a radiobox is checked or not
         let stock = 0;
 
-        if(req.body.stock === "on") {
+        if (req.body.stock === "on") {
             stock = 1;
         }
 
         // Update product with informations given
-        connection.query("UPDATE products SET name = ?, price = ?, in_stock = ?, image = ? WHERE id = ?", [req.body.product_name, parseInt(req.body.price), stock,  req.body.image, req.params.id]);
+        connection.execute("UPDATE products SET name = ?, price = ?, in_stock = ?, image = ? WHERE id = ?", [req.body.product_name, parseInt(req.body.price), stock, req.body.image, req.params.id]);
 
         // Redirect user back
         res.redirect("/manager");
